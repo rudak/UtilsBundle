cd048d4f67d535d675eaf9823cf87dadab1ceb7e
rudak <roiduboul@gmail.com>
2014-12-29 12:56:48 +0100

    update

---
+++ CustomCanonicalizer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rudak
+ * Date: 29/12/2014
+ * Time: 12:49
+ */
+
+namespace Rudak\UtilsBundle;
+
+use FOS\UserBundle\Util\CanonicalizerInterface;
+use Rudak\UtilsBundle\Slug;
+
+class CustomCanonicalizer implements CanonicalizerInterface
+{
+    public function canonicalize($string)
+    {
+        $Slug = new Slug($string);
+        return $Slug->getSlug();
+    }
+}
